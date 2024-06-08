eb5c2bc4358eb6b736e708c07cb43a0db15e7c5f
Aleksandar Ristic <storiaca@gmail.com>
2024-06-08 16:30:29 +0200

    Implement react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
@@ -9,7 +11,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
