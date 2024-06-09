4fb1315b1c513016764ed5b14ee83db976a0c13b
Aleksandar Ristic <storiaca@gmail.com>
2024-06-09 17:45:40 +0200

    Product details page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,18 +7,21 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
-import HomeScreen from "./screens/HomeScreen.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
