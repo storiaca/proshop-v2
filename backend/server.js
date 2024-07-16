ac8ef0749ab711c46b2942f10f0cddca7b64a852
Aleksandar Ristic <storiaca@gmail.com>
2024-07-16 13:20:37 +0200

    Get products from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 connectDB(); // Connect to MongoDB
 
@@ -14,14 +15,7 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
