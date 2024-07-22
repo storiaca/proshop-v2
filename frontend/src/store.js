d86457c442eb5cba74f496bde0d63ade0741adb4
Aleksandar Ristic <storiaca@gmail.com>
2024-07-22 18:55:04 +0200

    Cart slice and reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice";
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMidleware) =>
     getDefaultMidleware().concat(apiSlice.middleware),
